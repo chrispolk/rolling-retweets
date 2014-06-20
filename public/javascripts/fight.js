85686123632d6b9f5e203bbd96a60e3e9bfeefc2
Chris Polk <cchpolk@gmail.com>
2014-06-20 14:03:37 -0700

    minor tweak on conditions to add to storage

--- public/javascripts/fight.js
+++ public/javascripts/fight.js
@@ -92,7 +92,7 @@ var TopTenRetweetBox = React.createClass({
     
     if(!is_duplicate) {
       // if we haven't filled up the top 10 list with retweets or the most recent retweet's retweet count is greater than the current lowest
-      if(tweets.length < 10 || tweet.retweeted_status.retweet_count > tweets[tweets.length - 1].retweeted_status.retweet_count) {
+      if(tweets.length < 20 || tweet.retweeted_status.retweet_count > tweets[tweets.length - 1].retweeted_status.retweet_count) {
         tweet.retweeted_status.retweet_count = 1;
         var newTweets = tweets.concat([tweet]);
         newTweets.sort( function (a,b) { return b.retweeted_status.retweet_count - a.retweeted_status.retweet_count } );
